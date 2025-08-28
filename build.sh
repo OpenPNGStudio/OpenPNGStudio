d9b61dd325f643a48a2088fa570f765cb9eec7d5
LowByteFox <firethefox77@gmail.com>
2025-08-28 11:59:49 +0200

    1st attempt on gh actions

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ if [ "$(basename $(pwd))" = "build" ]; then
     cd ..
 fi
 
+RELEASE_BUILD="${RELEASE_BUILD:-false}"
+
 set -e
-c3c compile-run -o $(mktemp /tmp/XXXXXXX) build.c3 ./build-files/*
+
+c3c compile-run -o $(mktemp /tmp/XXXXXXX) build.c3 ./build-files/* -- $RELEASE_BUILD
