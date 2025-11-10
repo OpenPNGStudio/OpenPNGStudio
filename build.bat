900f46ecdbf68ef13f0d51e434ef038a7ad50a62
LowByteFox <firethefox77@gmail.com>
2025-11-10 08:41:39 +0100

    windows CI attempt

---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if /i "%RELEASE_BUILD%"=="true" (
+    set RELEASE_BUILD=RelWithDebInfo
+) else (
+    set RELEASE_BUILD=Debug
+)
+
+mkdir build
+cd build
+cmake -G "Ninja" -DCMAKE_GENERATOR_PLATFORM="" -DCMAKE_BUILD_TYPE="%RELEASE_BUILD%" -DCMAKE_INSTALL_PREFIX=".\miniroot" ..
+if errorlevel 1 exit /b %errorlevel%
+
+ninja
+if errorlevel 1 exit /b %errorlevel%
+
+ninja install
+if errorlevel 1 exit /b %errorlevel%
+
+c3c build OpenPNGStudio-windows-x64
+if errorlevel 1 exit /b %errorlevel%
