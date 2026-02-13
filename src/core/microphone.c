a76934bdcdf15797beab7cbfcdcc200714366a1a
LowByteFox <firethefox77@gmail.com>
2026-02-13 14:48:54 +0100

    implement microphone configuration

--- src/core/microphone.c
+++ src/core/microphone.c
@@ -1,10 +1,13 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <core/microphone.h>
 #include <math.h>
 #include <miniaudio.h>
 #include <stdatomic.h>
 
+static ma_context context;
 static ma_device device;
 static ma_device_config config;
 
@@ -14,6 +17,11 @@ static void on_data(ma_device* device, void* output, const void* input,
 void mic_setup(struct microphone *self)
 {
     ma_result result;
+
+    if (ma_context_init(NULL, 0, NULL, &context)) {
+        /* TODO: somehow allow C to log stuff */
+        return;
+    }
 
     config = ma_device_config_init(ma_device_type_capture);
     config.capture.format = ma_format_f32;
@@ -34,9 +42,64 @@ void mic_setup(struct microphone *self)
     //     LOG_E("Failed to start device.\n", 0);
 }
 
+struct device {
+    char *name;
+    ma_device_id *id;
+};
+
+void switch_device(struct microphone *self, struct device target)
+{
+    ma_result result;
+
+    ma_device_stop(&device);
+    ma_device_uninit(&device);
+
+    config = ma_device_config_init(ma_device_type_capture);
+    config.capture.pDeviceID = target.id;
+    config.capture.format = ma_format_f32;
+    config.capture.channels = 1;
+    config.sampleRate = 44100;
+    config.dataCallback = on_data;
+
+    result = ma_device_init(&context, &config, &device);
+    if (result != MA_SUCCESS) return;
+
+    device.pUserData = self;
+    result = ma_device_start(&device);
+}
+
+struct device *mic_enumerate(int *len)
+{
+    ma_result result;
+    ma_device_info *device_infos;
+    ma_uint32 count;
+
+    result = ma_context_get_devices(&context, NULL, NULL, &device_infos, &count);
+    if (result != MA_SUCCESS) {
+        return NULL;
+    }
+
+    int iter = 0;
+    struct device *devices = malloc(sizeof(*devices) * count);
+
+    for (int i = 0; i < count; i++) {
+        devices[iter].id = malloc(sizeof(ma_device_id));
+        *devices[iter].id = device_infos[i].id;
+        devices[iter].name = device_infos[i].name;
+
+        iter++;
+    }
+
+    *len = iter;
+
+    return devices;
+}
+
 void mic_free()
 {
+    ma_device_stop(&device);
     ma_device_uninit(&device);
+    ma_context_uninit(&context);
 }
 
 static void on_data(ma_device* device, void* output, const void* input,
