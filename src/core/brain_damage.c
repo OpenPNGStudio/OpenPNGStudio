db52c847ee378937cc9fd3ad51aeca4b83599304
LowByteFox <firethefox77@gmail.com>
2026-02-16 15:08:37 +0100

    builds on windows again

---
+++ src/core/brain_damage.c
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+/* Not really Microsoft to blame *pat pat*
+ * 
+ * Some C developers never heard the word "consistency"
+ */
+#include <nuklear.h>
+#include <stdio.h>
+
+nk_flags winabi_get_nk_flags(struct nk_context *ctx)
+{
+	return ctx->current->flags;
+}
+
+void winabi_add_nk_flag(struct nk_context* ctx, nk_flags flag)
+{
+	ctx->current->flags |= flag;
+}
+
+void winabi_set_nk_flag(struct nk_context* ctx, nk_flags flag)
+{
+	ctx->current->flags = flag;
+}
+
+static struct nk_vec2 winabi_panel_get_padding2(struct nk_style* style, enum nk_panel_type type)
+{
+    switch (type) {
+    default:
+    case NK_PANEL_WINDOW:
+        return style->window.padding;
+    case NK_PANEL_GROUP:
+        return style->window.group_padding;
+    case NK_PANEL_POPUP:
+        return style->window.popup_padding;
+    case NK_PANEL_CONTEXTUAL:
+        return style->window.contextual_padding;
+    case NK_PANEL_COMBO:
+        return style->window.combo_padding;
+    case NK_PANEL_MENU:
+        return style->window.menu_padding;
+    case NK_PANEL_TOOLTIP:
+        return style->window.menu_padding;
+    }
+
+    return nk_vec2(0, 0);
+}
+
+struct nk_vec2 winabi_panel_get_padding(struct nk_context* ctx)
+{
+    return winabi_panel_get_padding2(&ctx->style, ctx->current->layout->type);
+}
--- src/wrappers.c
+++ src/wrappers.c
@@ -1,6 +1,38 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
-
 #include <stdio.h>
+
+#ifdef _WIN32
+// move the windows functions to new names
+// note that you can't call these functions or structures from your code, but you should not neeed to
+#define CloseWindow CloseWindowWin32
+#define Rectangle RectangleWin32
+#define ShowCursor ShowCursorWin32
+#define LoadImageA LoadImageAWin32
+#define LoadImageW LoadImageWin32
+#define DrawTextA DrawTextAWin32
+#define DrawTextW DrawTextWin32
+#define DrawTextExA DrawTextExAWin32
+#define DrawTextExW DrawTextExWin32
+#define PlaySoundA PlaySoundAWin32
+// include windows
+#define WIN32_LEAN_AND_MEAN 
+#include <windows.h>
+
+// remove all our redfintions so that raylib can define them properly
+#undef CloseWindow
+#undef Rectangle
+#undef ShowCursor
+#undef LoadImage 
+#undef LoadImageA
+#undef LoadImageW
+#undef DrawText 
+#undef DrawTextA
+#undef DrawTextW
+#undef DrawTextEx 
+#undef DrawTextExA
+#undef DrawTextExW
+#undef PlaySoundA
+#endif
 
 /* just to improve compilation times */
 #define RAYLIB_NUKLEAR_IMPLEMENTATION
